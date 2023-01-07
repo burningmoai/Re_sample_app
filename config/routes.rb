6ff41c94defb076543d0567ea95f10cb42cf2f41
burningmoai <114802516+burningmoai@users.noreply.github.com>
2023-01-07 09:24:19 +0000

    [Start]復習開始

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  # get 'homes/top' URLと'コントローラ名#アクション名'が同じなら省略可能!!
+  get '/top' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
