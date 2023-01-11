8be0eecfb8323ee33723cf029470cdccde327733
burningmoai <114802516+burningmoai@users.noreply.github.com>
2023-01-11 14:59:54 +0000

    [Add]edit route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
+  get 'lists:id/edit' => 'lists#edit', as: 'edit_list'
   get 'lists' => 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
